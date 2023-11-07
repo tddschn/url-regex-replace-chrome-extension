b245349aa6c59f374d73dd2a1790e75386c388ce
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-11-07 16:21:18 -0500

    2023-11-07T16-21-17Z

--- popup.js
+++ popup.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const prependButton = document.getElementById("prepend");
+  const prefixInput = document.getElementById("prefix");
+
+  // Load the saved prefix value when the popup is opened.
+  chrome.storage.sync.get("savedPrefix", function (data) {
+    if (data.savedPrefix) {
+      prefixInput.value = data.savedPrefix;
+    }
+  });
+
+  // Listen for changes in the input field to save them.
+  prefixInput.addEventListener("change", function () {
+    const prefix = prefixInput.value;
+    chrome.storage.sync.set({ "savedPrefix": prefix });
+  });
 
   prependButton.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      const prefix = document.getElementById("prefix").value;
+      const prefix = prefixInput.value;
       const tabId = tabs[0].id;
       const currentUrl = tabs[0].url;
       const newUrl = prefix + currentUrl;
