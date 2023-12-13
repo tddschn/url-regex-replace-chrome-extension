b9322d5a047b8cb76ad1529a980aa9a5f8cf48b7
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-12-13 17:44:25 -0500

    2023-12-13T17-44-25Z

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.runtime.onInstalled.addListener(function (details) {
   if (details.reason == "install") {
     // This code runs only when the extension is first installed
     const defaultPresets = [
+      { find: "^", replace: "https://web.archive.org/web/" },
       { find: "^", replace: "https://archive.is/" },
       { find: "^https://", replace: "https://sourcegraph.com/" },
     ];
